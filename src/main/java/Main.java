af539a8b6fea7574ea3342d29f989752bf80d50c
PaCm4n <PaCm4n@test.ru>
2025-07-03 21:30:35 +0300

    Fix remarks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
+    public static final int MINSPEED = 0;
+    public static final int MAXSPEED = 250;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         HashSet<Car> cars = new HashSet<>();
@@ -15,11 +18,17 @@ public class Main {
 
             while (true) {
                 System.out.println("— Введите скорость машины №" + i +":  ");
-                speedCar = scanner.nextInt();
-                if (speedCar > 0 && speedCar <= 250) {
-                    break;
+                if (scanner.hasNextInt()) {
+                    speedCar = scanner.nextInt();
+                    if (speedCar > MINSPEED && speedCar <= MAXSPEED) {
+                        break;
+                    }
+                    System.out.println("Недопустимая скорость машины. Повторите ввод!");
+                } else {
+                    System.out.println("Недопустимая скорость машины. Повторите ввод!");
+                    scanner.next();
                 }
-                System.out.println("Недопустимая скорость машины. Повторите ввод!");
+
             }
             Car car = new Car(nameCar, speedCar);
             cars.add(car);
