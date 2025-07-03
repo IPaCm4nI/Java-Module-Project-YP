752d625e8db939ed90b7afbba8eebfd12f3aa2b1
PaCm4n <PaCm4n@test.ru>
2025-07-03 20:44:51 +0300

    Commit project №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        HashSet<Car> cars = new HashSet<>();
+
+        for (int i = 1; i <= 3; i++) {
+            String nameCar;
+            int speedCar;
+
+            System.out.println("— Введите название машины №" + i + ":  ");
+            nameCar = scanner.next();
+
+            while (true) {
+                System.out.println("— Введите скорость машины №" + i +":  ");
+                speedCar = scanner.nextInt();
+                if (speedCar > 0 && speedCar <= 250) {
+                    break;
+                }
+                System.out.println("Недопустимая скорость машины. Повторите ввод!");
+            }
+            Car car = new Car(nameCar, speedCar);
+            cars.add(car);
+        }
+
+        Race race = new Race();
+        System.out.println("Самая быстрая машина: " + race.startRace(cars));
     }
 }
---
+++ src/main/java/Race.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class Race {
+    String leader;
+
+    public String startRace(HashSet<Car> cars) {
+        int maxDistantce = 0;
+
+        for (Car car : cars) {
+            int distance = 24 * car.speed;
+            if (distance > maxDistantce) {
+                this.leader = car.name;
+            }
+        }
+
+        return leader;
+    }
+}
